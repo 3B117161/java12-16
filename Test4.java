f850ac63bc4cc5ca9e677e49aced0a66c9efaa65
3B117161 <114204288+3B117161@users.noreply.github.com>
2022-12-16 12:02:29 +0800

    Add files via upload
    //2022/12/16最後一題找出最大值最小值計算平均值中位數

---
+++ Test4.java
@@ -0,0 +1,23 @@
+public class Test4{
+	public static void main (String[] args){
+		int [] myArr = {7,5,4,8,1,2};
+		int [] res = new int [myArr.length];
+		
+		for (int i = 0,j = res.length-1; i <myArr.length; i++,j--){
+			res[j]=myArr[i];
+			//找出最大值
+			//找出最小值
+			//計算平均值
+			//計算中位數
+		}
+		for(int a =0; a<myArr.length;a++){
+			System.out.println(res[a]);
+							//印出 最大值
+			//印出 最小值
+			//印出 平均值
+			//印出 中位數
+		}
+	}	
+}
+//2022/12/16最後一題找出最大值最小值計算平均值中位數
+		}
