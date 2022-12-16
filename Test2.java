8065f53f2e4f9f3b6c20ad9ecd8cfd716042a248
3B117161 <114204288+3B117161@users.noreply.github.com>
2022-12-16 10:37:56 +0800

    Add files via upload
    //陣列印出754812
    //參考網站https://toyangel.pixnet.net/blog/post/27143246-%E3%80%90java%E3%80%91-array---%E5%8D%B0%E5%87%BA%E9%99%A3%E5%88%97%E7%9A%84%E5%80%BC

---
+++ Test2.java
@@ -0,0 +1,13 @@
+public class Test2{
+	public static void main (String[] args){
+		int[] myArr = new int[] {7,5,4,8,1,2};
+
+		for(int e : myArr){
+            System.out.print(e);
+        }
+
+    }
+}
+//陣列印出754812
+//參考網站https://toyangel.pixnet.net/blog/post/27143246-%E3%80%90java%E3%80%91-array---%E5%8D%B0%E5%87%BA%E9%99%A3%E5%88%97%E7%9A%84%E5%80%BC
+
